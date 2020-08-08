4f8eb1291f835697a870da591fb5f108961baafb
Alana Corrêa <alanarcorrea.atlas@gmail.com>
2020-08-07 22:28:55 -0300

    :construction: integrated with api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,34 +2,53 @@ import React from 'react';
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './style.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number,
+    avatar: string, 
+    bio: string, 
+    cost: number, 
+    name: string, 
+    subject: string,
+    whatsapp: string, 
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/15933128?s=460&u=d09ec6ff66f122451587c5a434586d59a99bc216&v=4" 
-                alt="Alana Corrêa" />
+                <img src={teacher.avatar}
+                alt={teacher.name} />
                 <div>
-                    <strong>Alana Corrêa</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias do mundo.
-                <br /> <br/>
-                Apaixonada por codar códigos malucos e inteligentes. Coisas que possam melhorar a vida das pessoas e 
-                facilitar seu dia a dia. Mais de 50000.0000 pessoas já tiveram sua vida impactada por mim.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
-                    <img src={whatsAppIcon} alt="Whatsapp"/>
-                    Entrar em contato
-                </button>
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
+                        <img src={whatsAppIcon} alt="Whatsapp"/>
+                        Entrar em contato
+                </a>
             </footer>
         </article>
     )
